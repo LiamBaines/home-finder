8e5165618e3fad68cfc7f5ce9fd9c6914c8ca796
Liam Baines <liam@thebaines.com>
2021-11-29 22:17:18 +0000

    Add bearings

--- home-finder/src/App.js
+++ home-finder/src/App.js
@@ -3,24 +3,10 @@ import Location from './location/Location.js';
 
 import Wrap from './wrap/Wrap.js'
 
+import calculateBearing from './maps/calculateBearing.js';
 import fetchCoords from './maps/fetchCoords.js';
 
 import React from 'react';
-
-function beta(lat1, lon1, lat2, lon2) {
-
-  const φ1 = lat1 * Math.PI/180; // φ, λ in radians
-  const φ2 = lat2 * Math.PI/180;
-  const λ1 = lon1 * Math.PI/180;
-  const λ2 = lon2 * Math.PI/180;
-
-  const y = Math.sin(λ2-λ1) * Math.cos(φ2);
-  const x = Math.cos(φ1) * Math.sin(φ2) -
-            Math.sin(φ1) * Math.cos(φ2)* Math.cos(λ2-λ1);
-  const θ = Math.atan2(y, x);
-
-  return (θ * 180 / Math.PI + 360) % 360
-}
 
 class App extends React.Component {
 
@@ -28,8 +14,9 @@ class App extends React.Component {
     super(props);
     this.state = {
       locations: [
-        new Location("Buenos Aires", -34.6, -58.4),
-        new Location("Vauxhall", 51.5, -0.12)
+        new Location("Brixton, London", 51.4613, -0.1156, 0),
+        new Location("Buenos Aires", -34.6037, -58.3816, 225.3122),
+        new Location("Vauxhall, London", 51.4862, -0.1217, 351.3264)
       ]
     }
   }
@@ -38,7 +25,7 @@ class App extends React.Component {
     for (let i = 0; i < this.state.locations.length; i++) {
       fetchCoords(this.state.locations[i].name).then(location => {
         this.setState((curr, props) => ({
-          locations: curr.locations.map((item, j) => i === j ? new Location(this.state.locations[i].name, location.lat, location.lng) : item)
+          locations: curr.locations.map((item, j) => i === j ? new Location(this.state.locations[i].name, location.lat, location.lng, calculateBearing(this.state.locations[0], location)) : item)
         }))
       })
     }
@@ -46,30 +33,17 @@ class App extends React.Component {
 
   updateName(i, str) {
     this.setState((curr, props) => ({
-      locations: curr.locations.map((location, j) => i === j ? new Location(str, location.lat, location.lng) : location)
+      locations: curr.locations.map((location, j) => i === j ? new Location(str, location.lat, location.lng, location.bearing) : location)
     }))
   }
 
   addLocation() {
     this.setState(curr => ({
-      locations: curr.locations.concat(new Location("", 0, 0))
+      locations: curr.locations.concat(new Location("", 0, 0, 0))
     }))
   }
 
   render() {
-
-    // let x = 51.4613;
-    // let y = -0.1156;
-
-    // console.log("beta(Brixton, Buenos Aires) = " + beta(x, y, -34.6037, -58.3816))
-    // console.log("beta(Brixton, Vauxhall) = " + beta(x, y, 51.4862, -0.1217))    
-    // console.log("beta(Brixton, New Delhi) = " + beta(x, y, 28.6139, 77.2090))    
-    // console.log("beta(Brixton, Washington D.C.) = " + beta(x, y, 38.9072, -77.0369))    
-    // console.log("beta(Brixton, Paris) = " + beta(x, y, 48.8566, 2.3522))    
-    // console.log("beta(Brixton, Holloway) = " + beta(x, y, 51.5570, -0.1155))    
-    // console.log("beta(Brixton, Cambridge) = " + beta(x, y, 52.2053, 0.1218))    
-    // console.log("beta(Brixton, Madrid) = " + beta(x, y, 40.4168, -3.7038))        
-    // console.log("beta(Brixton, Santiago) = " + beta(x, y, -33.4489, -70.6693)) 
     return (
         Wrap({
           locations: this.state.locations,
--- home-finder/src/wrap/Wrap.js
+++ home-finder/src/wrap/Wrap.js
@@ -3,17 +3,26 @@ import LocationInput from '../location/LocationInput.js';
 import React from 'react';
 
 function Wrap(props) {
-  let locations = props.locations.map((location, i) => new LocationInput(location, props.updateName, i));
+  let locations = props.locations.map((location, i) => new LocationInput(location, i, props.updateName));
   return (   
     <div>
       <header>
         <div id="wrap" className="container main">
-          <div className="form-inline">
-            {locations}
+          <div class="row">
+            <div class="col">
+              Hello
+            </div>
           </div>
-          <div>
-          <button onClick={props.addLocation}>Add location</button>
-            <button onClick={props.updateCoords}>Update coordinates</button>
+          <div class="row">
+            <div class="col">
+              <div className="form-inline">
+                {locations}
+              </div>
+              <div>
+                <button onClick={props.addLocation}>Add location</button>
+                <button onClick={props.updateCoords}>Update coordinates</button>
+              </div>
+            </div>
           </div>
         </div>
       </header>
